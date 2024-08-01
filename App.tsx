1d52c14b1ef04d3ab4d6f4263e5dd02dcb7c123f
Romario Kavin <98270892+RomarioKavin1@users.noreply.github.com>
2024-08-01 21:59:19 +0530

    fixed

--- App.tsx
+++ App.tsx
@@ -12,19 +12,29 @@ import {
 const ThingSpeakDataDisplay = () => {
   const [sensorData, setSensorData] = useState(0);
   const [connectionStatus, setStatus] = useState('');
+  const [sensorData2, setSensorData2] = useState(0);
+  const [connectionStatus2, setStatus2] = useState('');
   const [maxLevel, setMaxLevel] = useState('');
   const [minLevel, setMinLevel] = useState('');
   const [height, setHeight] = useState('');
+  const [maxLevel2, setMaxLevel2] = useState('');
+  const [minLevel2, setMinLevel2] = useState('');
+  const [height2, setHeight2] = useState('');
 
   const [alertMessage, setAlertMessage] = useState('');
+  const [alertMessage2, setAlertMessage2] = useState('');
 
   useEffect(() => {
     loadData();
-    fetchData();
-    fetchConnection();
+    fetchData1();
+    fetchConnection1();
+    fetchData2();
+    fetchConnection2();
     const interval = setInterval(() => {
-      fetchData();
-      fetchConnection();
+      fetchData1();
+      fetchConnection1();
+      fetchData2();
+      fetchConnection2();
     }, 1000);
 
     return () => clearInterval(interval);
@@ -45,16 +55,36 @@ const ThingSpeakDataDisplay = () => {
       }
     }
   }, [sensorData, maxLevel, minLevel]);
+  useEffect(() => {
+    if (sensorData2 !== undefined && maxLevel2 !== '' && minLevel2 !== '') {
+      if (sensorData2 > maxLevel2) {
+        setAlertMessage2(
+          `Warning: Sensor reading (${sensorData2}) is above the maximum level (${maxLevel2})!`,
+        );
+      } else if (sensorData2 < minLevel2) {
+        setAlertMessage2(
+          `Warning: Sensor reading (${sensorData2}) is below the minimum level (${minLevel2})!`,
+        );
+      } else {
+        setAlertMessage2('');
+      }
+    }
+  }, [sensorData2, maxLevel2, minLevel2]);
 
   const loadData = async () => {
     try {
       const storedHeight = await AsyncStorage.getItem('height');
       const storedMaxLevel = await AsyncStorage.getItem('maxLevel');
       const storedMinLevel = await AsyncStorage.getItem('minLevel');
-
+      const storedHeight2 = await AsyncStorage.getItem('height2');
+      const storedMaxLevel2 = await AsyncStorage.getItem('maxLevel2');
+      const storedMinLevel2 = await AsyncStorage.getItem('minLevel2');
       if (storedHeight !== null) setHeight(storedHeight);
       if (storedMaxLevel !== null) setMaxLevel(storedMaxLevel);
       if (storedMinLevel !== null) setMinLevel(storedMinLevel);
+      if (storedHeight2 !== null) setHeight2(storedHeight2);
+      if (storedMaxLevel2 !== null) setMaxLevel2(storedMaxLevel2);
+      if (storedMinLevel2 !== null) setMinLevel2(storedMinLevel2);
     } catch (error) {
       console.error('Failed to load data from local storage:', error);
     }
@@ -65,6 +95,9 @@ const ThingSpeakDataDisplay = () => {
       await AsyncStorage.setItem('height', height);
       await AsyncStorage.setItem('maxLevel', maxLevel);
       await AsyncStorage.setItem('minLevel', minLevel);
+      await AsyncStorage.setItem('height2', height2);
+      await AsyncStorage.setItem('maxLevel2', maxLevel2);
+      await AsyncStorage.setItem('minLevel2', minLevel2);
     } catch (error) {
       console.error('Failed to save data to local storage:', error);
     }
@@ -83,9 +116,43 @@ const ThingSpeakDataDisplay = () => {
       console.error('Failed to reset data in local storage:', error);
     }
   };
+  const resetData2 = async () => {
+    setHeight2('');
+    setMaxLevel2('');
+    setMinLevel2('');
 
-  const fetchData = async () => {
-    const url = `https://level-sensor-1663d-default-rtdb.asia-southeast1.firebasedatabase.app/readings.json`;
+    try {
+      await AsyncStorage.removeItem('height2');
+      await AsyncStorage.removeItem('maxLevel2');
+      await AsyncStorage.removeItem('minLevel2');
+    } catch (error) {
+      console.error('Failed to reset data in local storage:', error);
+    }
+  };
+
+  const fetchData2 = async () => {
+    const url = `https://level-sensor-1663d-default-rtdb.asia-southeast1.firebasedatabase.app/sensor_2/readings.json`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setSensorData2(data.value);
+    } catch (error) {
+      console.error('Failed to fetch data from ThingSpeak:', error);
+    }
+  };
+
+  const fetchConnection2 = async () => {
+    const url = `https://level-sensor-1663d-default-rtdb.asia-southeast1.firebasedatabase.app/sensor_2/on_status.json`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      setStatus2(data.on_status);
+    } catch (error) {
+      console.error('Failed to fetch data from ThingSpeak:', error);
+    }
+  };
+  const fetchData1 = async () => {
+    const url = `https://level-sensor-1663d-default-rtdb.asia-southeast1.firebasedatabase.app/sensor_1/readings.json`;
     try {
       const response = await fetch(url);
       const data = await response.json();
@@ -95,8 +162,8 @@ const ThingSpeakDataDisplay = () => {
     }
   };
 
-  const fetchConnection = async () => {
-    const url = `https://level-sensor-1663d-default-rtdb.asia-southeast1.firebasedatabase.app/on_status.json`;
+  const fetchConnection1 = async () => {
+    const url = `https://level-sensor-1663d-default-rtdb.asia-southeast1.firebasedatabase.app/sensor_1/on_status.json`;
     try {
       const response = await fetch(url);
       const data = await response.json();
@@ -107,6 +174,7 @@ const ThingSpeakDataDisplay = () => {
   };
 
   const calculatedReading = height ? height - sensorData : 'N/A';
+  const calculatedReading2 = height2 ? height2 - sensorData2 : 'N/A';
 
   return (
     <PaperProvider>
@@ -156,6 +224,53 @@ const ThingSpeakDataDisplay = () => {
               </View>
             ) : null}
             <Paragraph>Sensor ID: {1}</Paragraph>
+          </Card.Content>
+        </Card>
+        <Card key={2} style={styles.card}>
+          <Card.Content>
+            <Paragraph style={styles.txt}>
+              Battery Percentage: {connectionStatus2 || 'N/A'}
+            </Paragraph>
+            <Paragraph style={styles.txt}>
+              Readings: {calculatedReading2}
+            </Paragraph>
+            <Card key={5} style={styles.card2}>
+              <TextInput
+                label="Height"
+                value={height2}
+                onChangeText={setHeight2}
+                keyboardType="numeric"
+              />
+              <Button onPress={saveData}>Save</Button>
+              <Button onPress={resetData2}>Reset</Button>
+            </Card>
+            <Card key={2} style={styles.card2}>
+              <TextInput
+                label="Max Level"
+                value={maxLevel2}
+                onChangeText={setMaxLevel2}
+                keyboardType="numeric"
+              />
+              <Button onPress={saveData}>Save</Button>
+              <Button onPress={resetData}>Reset</Button>
+            </Card>
+            <Card key={3} style={styles.card2}>
+              <TextInput
+                label="Min Level"
+                value={minLevel2}
+                onChangeText={setMinLevel2}
+                keyboardType="numeric"
+              />
+              <Button onPress={saveData}>Save</Button>
+              <Button onPress={resetData}>Reset</Button>
+            </Card>
+
+            {alertMessage2 ? (
+              <View style={styles.alertContainer}>
+                <Text style={styles.alertText}>{alertMessage2}</Text>
+              </View>
+            ) : null}
+            <Paragraph>Sensor ID: {2}</Paragraph>
           </Card.Content>
         </Card>
       </ScrollView>
